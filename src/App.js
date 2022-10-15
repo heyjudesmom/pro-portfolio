194e98e4a9f71d577f7e16e583d7145383abdf02
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-10-15 15:11:46 -0400

    HashRouter and basic nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <nav>
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+         </ul>
+     </nav>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+      </Routes>
+    </main>
   );
 }
 
