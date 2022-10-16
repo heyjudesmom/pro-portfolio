c7fd0fc33d20e4ab0c7afc4097adf1a2266c3467
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-10-16 15:26:36 -0400

    Project technology list

--- src/Components/ProjectDetail/ProjectDetail.jsx
+++ src/Components/ProjectDetail/ProjectDetail.jsx
@@ -3,6 +3,9 @@ export default function ProjectDetail({ p, i }) {
         const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
         if (newWindow) newWindow.opener = null
       }
+      let str = p.technologies.map(function(t) {
+        return <div>{t}</div>
+      })
     return (
             <div className="col s12 m6">
                 <div className="card">
@@ -17,7 +20,7 @@ export default function ProjectDetail({ p, i }) {
                     <div className="card-reveal">
                         <span class="card-title grey-text text-darken-4">{p.name}<i class="material-icons right">close</i></span>
                         <p>{p.description}</p>
-                        <p>{p.technologies}</p>
+                        <p>{str}</p>
                         <a className="btn-floating waves-effect waves-light black" onClick={() => openInNewTab(`${p.src}`)}><img className="material-icons" src="https://i.imgur.com/Dl5An0s.png" alt="GitHub Logo" height="40px"/></a>
                         <a className="btn-floating waves-effect waves-light black" onClick={() => openInNewTab(`${p.url}`)}><i className="material-icons">open_in_new</i></a>
                     </div>
