eec40e888ed36c270d0deadb7df603b38c7e6c52
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-10-16 15:15:26 -0400

    Add Tech Logos Component

---
+++ src/Components/TechIcons/TechIcons.jsx
@@ -0,0 +1,25 @@
+export default function TechIcons({ p, i }) {
+    const techUrls = [
+        "https://www.zencos.com/wp-content/uploads/2021/11/aws-logo-768x576.png", 
+        "https://www.pngkey.com/png/detail/142-1424654_we-help-you-to-solve-any-issues-or.png",
+        "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png",
+        "https://static.djangoproject.com/img/logos/django-logo-positive.png", 
+        "https://cdn0.iconfinder.com/data/icons/social-network-9/50/22-512.png", 
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/1200px-Javascript-shield.svg.png",
+        "https://seeklogo.com/images/M/materialize-logo-0FCAD8A6F8-seeklogo.com.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/2560px-MongoDB_Logo.svg.png",
+        "https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png",
+        "https://openwhisk.apache.org/images/runtimes/icon-python-text-color-horz.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        ]
+    const imgs = techUrls.map(function(url) {
+        return (
+            <>
+            <img src={url} alt="icon" style={{height: "10vmin", backgroundColor:"var(--white)", marginInline:"1vh"}}/>
+            </>
+        )
+    })
+    return (
+            <div>{imgs}</div>
+    )
+}
--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,23 +1,24 @@
+import TechIcons from "../../Components/TechIcons/TechIcons"
 export default function AboutPage() {
     return (
         <>
-        <div className="about-img"><img src="https://i.imgur.com/e81lLtK.png" alt="Photo of me" style={{ height: "30vh", borderRadius: "50%", border: "1vmin solid var(--dark-green)"}} /></div>
+            <div className="about-img"><img src="https://i.imgur.com/e81lLtK.png" alt="Photo of me" style={{ height: "30vh", borderRadius: "50%", border: "1vmin solid var(--dark-green)" }} /></div>
             <div>
                 <h3 className="h3-chalk">Meet the Engineer</h3>
-                <h3>Hi!</h3>
-                <p>I am a full stack developer.</p>
-                <p>I am a frontend engineer.</p>
-                <p>I am a backend engineer.</p>
-                <p>I am always expanding my skills.</p>
+                <div className="card">
+
+                    <h4>Hello!</h4>
+                    <p>I'm Brianna McCarthy and I am a full stack, front end, and back end engineer based in Portland, ME. In my previous career as a teacher, I adapted content to be accessible for all users. Teaching inspired me to learn more about technology resources and I became passionate about the educational technology experience.
+                    I am looking to work in a developer role with an collaborative team working to create intuitive experiences for clients.
+                    </p>
+                </div>
             </div>
             <div style={{ backgroundColor: "var(--dark-green)", color: "var(--white)" }}>
-                <h3>Experienced with:</h3>
-                <p>writing: Javascript, Python </p>
-                <p>frontend: React, CSS, HTML</p>
-                <p>backend: MongoDB, AWS, Django, PostgreSQL</p>
-                <p>styling: Bootstrap, Materialize, Pure, responsive design</p>
+                <h4>Experienced with:</h4>
+                <TechIcons />
+                <h6>and I am always expanding my skills!</h6>
             </div>
-            <div style={{backgroundColor: "var(--green)"}}>
+            <div style={{ backgroundColor: "var(--green)" }}>
                 <h3>Inspired by:</h3>
                 <p>Collaboration</p>
                 <p>Real-World Problems</p>
